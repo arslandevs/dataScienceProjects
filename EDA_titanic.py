11ad7cc30c75437e32f10d1ef2ebdd7aa0ed49df
SyedArsalanAmin <sarsalanamin12@gmail.com>
2021-07-08 21:07:45 +0500

    Exploring-data1

--- EDA_titanic.py
+++ EDA_titanic.py
@@ -3,3 +3,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 %matplotlib inline
+
+df = pd.read_csv("E:\\DataScience & AI\\Github_repo\\DataScience-projects\\titanic_train.csv")
+df.head()
+
+df['Survived'].value_counts()
+
+df.info()
+
+df.describe()
+
+df.isna()
+
+
+df[df['Age'].isna()]
+
+df.dtypes
+
+df.isnull()
+
+# to see the missing values in a proper way use seaborn
+# here you can see the cabin and age has missing values but cabin has more.
+sns.heatmap(df.isna(), yticklabels=False, cmap='YlGnBu')
+plt.savefig("E:\DataScience & AI\Github_repo\DataScience-projects\missing_val.png")
