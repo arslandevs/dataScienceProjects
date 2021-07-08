8481f28c2be945de1f782e35c01051809a5f32cb
SyedArsalanAmin <sarsalanamin12@gmail.com>
2021-07-08 21:48:38 +0500

    Visualization1

--- EDA_titanic.py
+++ EDA_titanic.py
@@ -24,5 +24,22 @@ df.isnull()
 
 # to see the missing values in a proper way use seaborn
 # here you can see the cabin and age has missing values but cabin has more.
+sns.set_style("whitegrid")
 sns.heatmap(df.isna(), yticklabels=False, cmap='YlGnBu')
 plt.savefig("E:\DataScience & AI\Github_repo\DataScience-projects\missing_val.png")
+
+sns.countplot('Survived', data=df, hue='Sex')
+
+sns.countplot('Pclass', data=df, hue='Sex', palette='rainbow')
+
+sns.jointplot('Age', 'Survived', data=df, kind='hex')
+
+sns.jointplot('Age', 'Survived', data=df, kind='reg')
+
+
+sns.distplot(df['Age'].dropna(), kde=False, bins=50, color='black')
+
+sns.countplot('SibSp', data=df)
+
+
+sns.jointplot('Fare', 'Age', data=df, kind='reg')
